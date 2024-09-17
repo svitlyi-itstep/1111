e1ceb61d50521fecc4d8f941ed3f15a1865d6407
svitlyi <svitlyi_a@itstep.academy>
2024-09-17 17:52:07 +0300

    Changes at lesson 2

---
+++ Lesson_2/character.py
@@ -0,0 +1,16 @@
+class Character:
+    name = ''
+    health = 100
+    damage = 1
+    defence = 0
+
+    def __init__(self, name, health, damage, defence):
+        self.name = name
+        self.health = health
+        self.damage = damage
+        self.defence = defence
+
+    def show_stats(self):
+        print(f"-- {self.name} --\nЗдоров'я: {self.health}\n"
+              f"Шкода: {self.damage}\nЗахист: {self.defence}")
+
